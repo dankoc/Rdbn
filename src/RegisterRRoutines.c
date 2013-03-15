0d2701bade8004729fbee5140f053e364f2cc7df
Charles Danko <dankoc@gmail.com>
2013-03-15 15:25:47 -0400

    Common files init.

--- src/RegisterRRoutines.c
+++ src/RegisterRRoutines.c
@@ -1,6 +1,3 @@
-//using namespace std;
-//extern "C" {
-
 /**************************************************************
  *
  *      Associates a vector of genomic featuers (e.g. genes, CpG islands, etc.) with a table of sequence reads.
@@ -14,9 +11,6 @@
 
 
 SEXP compute_priors(SEXP all_posteriors);
-SEXP update_mean(SEXP tf_posterior, SEXP loci_bed, SEXP module_length, SEXP bin_width, SEXP bigwig_file);
-SEXP estimate_tf_binding(SEXP loci_bed, SEXP p_z, SEXP param, SEXP bg_param, SEXP model_type, SEXP window_size, SEXP bigwig_files);
-SEXP RnbinomMLE(SEXP n, SEXP weights, SEXP k);
 
 /**************************************************************
  *
@@ -26,13 +20,9 @@ SEXP RnbinomMLE(SEXP n, SEXP weights, SEXP k);
 void R_init_GROseq(DllInfo *info) {
      R_CallMethodDef callMethods[]  = {
        {"compute_priors", (DL_FUNC)&compute_priors, 1},
-       {"update_mean", (DL_FUNC)&update_mean, 5},
-       {"estimate_tf_binding", (DL_FUNC)&estimate_tf_binding, 7},
-	   {"RnbinomMLE", (DL_FUNC)&estimate_tf_binding, 3},
        {NULL, NULL, 0}
      };
 
         R_registerRoutines(info, NULL, callMethods, NULL, NULL);
 }
 
-//}
