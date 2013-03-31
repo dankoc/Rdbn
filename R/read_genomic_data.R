56101f7cef3ba354e1d6bd999e8d49855e0f33da
dankoc <dankoc@gmail.com>
2013-03-31 14:37:58 -0400

    Fixed bugs in previous push.

--- R/read_genomic_data.R
+++ R/read_genomic_data.R
@@ -4,19 +4,19 @@
 #' Reads genomic data from the specified position...
 #'
 #' @param bed A data.frame of genomic regions in bed format (chrom, start, end).
-#' @param big_wig_plus Path to bigwig file representing GRO-seq/ PRO-seq reads on the plus strand.
-#' @param big_wig_minus Path to bigwig file representing GRO-seq/ PRO-seq reads on the minus strand.
+#' @param bigwig_plus Path to bigwig file representing GRO-seq/ PRO-seq reads on the plus strand.
+#' @param bigwig_minus Path to bigwig file representing GRO-seq/ PRO-seq reads on the minus strand.
 #' @param as_matrix If true, returns a matrix object.
 #' @param window_sizes Size of windows in the model.
 #' @param half_nWindows Number of windows on each side of the center position to evaluate.
 #' @return Returns a list() object, where each element in the list is the zoom data
 #' centered on a 
-read_genomic_data <- function(bed, big_wig_plus, big_wig_minus, as_matrix= TRUE, window_sizes= c(10, 50, 500), half_nWindows= c(20, 20, 20)) {
+read_genomic_data <- function(bed, bigwig_plus, bigwig_minus, as_matrix= TRUE, window_sizes= c(10, 50, 500), half_nWindows= c(20, 20, 20)) {
 
   stopifnot(NROW(window_sizes) == NROW(half_nWindows))
   zoom<- list(as.integer(window_sizes), as.integer(half_nWindows))
 
-  .Call("get_genomic_data_R", as.character(bed[,1]), as.integer((bed[,3]+bed[,2])/2), as.character(plus), as.character(minus), zoom, PACKAGE= "Rdbn")
+  dat <- .Call("get_genomic_data_R", as.character(bed[,1]), as.integer((bed[,3]+bed[,2])/2), as.character(bigwig_plus), as.character(bigwig_minus), zoom, PACKAGE= "Rdbn")
 
   if(as_matrix) 
     dat <- t(matrix(unlist(dat), ncol=NROW(bed)))
