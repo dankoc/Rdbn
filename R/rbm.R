9295f6c4b4f71b1de0713c064b132950c9c3223f
Charles Danko <dankoc@gmail.com>
2013-04-04 13:21:28 -0400

    Re-added clamp input/ output R functions.

--- R/rbm.R
+++ R/rbm.R
@@ -69,6 +69,68 @@ setGeneric("train",
 setMethod("train", c(rbm="rbm"), 
   function(rbm, data, batch_size=10, cdn= 1) { ## Regularization/ LASSO type options?!
   	stopifnot(NROW(data) == rbm@n_inputs)
+	
+	## Reassign input biases to training examples?!  As suggested in: http://www.cs.toronto.edu/~hinton/absps/guideTR.pdf
+	
+	
+	## Pass to C for training.
     .Call("train_rbm_R", rbm, as.real(data), as.integer(batch_size), as.integer(cdn), package="Rdbn") 
 })
 
+### Brought these back for now. Perhaps useful?! ##########################################################################################
+  
+#` 'Clamps' the input vector and runs the boltzmann machine to get the output node (i.e. get output conditional on specified input).
+setGeneric("clamp_input", 
+  def=function(rbm, set_input, ...) {
+	stopifnot(class(rbm) == "rbm")
+	standardGeneric("clamp_input")
+  })
+  
+setMethod("clamp_input", c(rbm="rbm"), 
+  function(rbm, set_input, return_prob= FALSE) {
+  	stopifnot(NROW(set_input) == rbm@n_inputs)
+	                                                      ## Or is it transpose of this?!
+	pr_act <- logistic_function(rbm@bias_outputs + set_input %*% rbm@io_weights) 
+
+	if(return_prob) return(pr_act)
+	else return(sample_states(pr_act))
+  })
+  
+    
+#` 'Clamps' the output vector and runs the boltzmann machine to get the output node (i.e. get expected input conditional on the specified output).
+setGeneric("clamp_output", 
+  def=function(rbm, set_output, ...) { 
+	stopifnot(class(rbm) == "rbm")
+	standardGeneric("clamp_output")
+  })
+  
+## Returns a prob. by default, as suggested in S3.2 of guideTR.pdf.
+setMethod("clamp_output", c(rbm="rbm"), 
+  function(rbm, set_output, return_prob= TRUE) {
+  	stopifnot(NROW(set_output) == rbm@n_outputs)
+	                                                   ## Is this even close to right?!
+	pr_act <- logistic_function(rbm@bias_inputs + set_output %*% rbm@io_weights) 
+
+	if(return_prob) return(pr_act)
+	else return(sample_states(pr_act))
+  })
+
+##################################################################
+## No reason to make these descend directly from the rbm class...
+
+## Returns a vector of probabilities by sampling the input vect_pr.
+#setGeneric("sample_states", def=function(rbm, vect_pr) {standardGeneric("sample_states")} )
+#setMethod("sample_states", c(rbm="rbm"), 
+sample_states <- function(rbm, vect_pr) {
+    vect_pr > runif(NROW(vect_pr))
+}#)
+
+## Returns the output of the logicstic function.
+#setGeneric("logistic_function", def=function(rbm, vect_val) {standardGeneric("logistic_function")} )
+#setMethod("logistic_function", c(rbm="rbm"), 
+logistic_function <- function(rbm, vect_val) {
+    return(1/(1+exp(-vect_val)))
+}#)
+
+
+#     x %*% y
