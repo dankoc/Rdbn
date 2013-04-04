644a8b14a9a621dc1c2fc1bb9bb8fafc6c399e22
Charles Danko <dankoc@gmail.com>
2013-04-03 22:59:36 -0400

    Minor change to test function!

--- test_functions/test_rbm.R
+++ test_functions/test_rbm.R
@@ -9,7 +9,15 @@
  training_data <- data.frame(HarryPotter=c(1,1,1,0,0,0), Avatar=c(1,0,1,0,0,0), LOTR3=c(1,1,1,1,1,1), 
 							Gladiator=c(0,0,0,1,1,1),Titanic=c(0,0,0,1,1,1),Glitter=c(0,0,0,0,0,0))
  row.names(training_data) <- c("Alice","Bob","Carol","David","Eric","Fred")
- data_matrix <- matrix(rep(data.matrix(training_data), 100), ncol=6) ## Convert to a matrix and repeat 100 times (for training).
+ training_data <- t(training_data)
+ 
+ ## Convert to a matrix and repeat 100 times (for training).
+ data_matrix <- matrix(data.matrix(training_data), nrow=6)[,sample(c(1:6), 1000, replace=TRUE)]
  
  rr <- rbm(n_inputs= as.integer(6), n_outputs= as.integer(2))
- train(rr, data= data_matrix, batch_size=1, cdn=1)
+ train(rr, data= data_matrix, batch_size=10, cdn=1)
+
+ 
+ ## NOTES: 
+ # (1) My batch size is affecting my weights.  I don't think that should be so!
+ # (2) 
