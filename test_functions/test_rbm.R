f84e39cf730dc5659fe9a4780c58d7de06e96c59
Charles Danko <dankoc@gmail.com>
2013-04-07 16:44:00 -0400

    Minor test function changes...

--- test_functions/test_rbm.R
+++ test_functions/test_rbm.R
@@ -12,9 +12,9 @@
  training_data <- t(training_data)
  
  ## Convert to a matrix and repeat 100 times (for training).
- data_matrix <- matrix(data.matrix(training_data), nrow=6)[,sample(c(1:6), 100, replace=TRUE)]
+ data_matrix <- matrix(data.matrix(training_data), nrow=6)[,sample(c(1:6), 10, replace=TRUE)]
  
- rr <- rbm(n_inputs= 6, n_outputs= 2, batch_size=10, cd_n=1)
+ rr <- rbm(n_inputs= 6, n_outputs= 2, batch_size=10, cd_n=1, momentum_decay= 0.9) #NA
  rbm.train(rr, data= data_matrix, n_epocs= 10000)
 
  #clamp_output(rr, c(0,1)) ## Currently dosen't work...
