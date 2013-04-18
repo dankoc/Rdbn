7ffc23eb6a4f04f322807695524b8c38c254ae95
Charles Danko <dankoc@gmail.com>
2013-04-18 13:17:35 -0400

    added test_Vowel.R.  Logistic function is now R accessible.

---
+++ test_functions/test_Vowel.R
@@ -0,0 +1,30 @@
+require(mlbench)
+data(Vowel)
+
+## Scale all variables to [0,1].
+x <- t(Vowel[,c(2:10)])
+y <- Vowel[,11]
+for(i in c(1:(NROW(x)))) {
+  x[i,] <- (x[i,]-min(x[i,]))/(max(x[i,])-min(x[i,]))
+}
+
+## Train a deep belief network.
+require(Rdbn)
+db <- dbn(n_layers= 4, layer_sizes= c(9,50,50,100), batch_size=100, cd_n=1, momentum_decay= 0.99, learning_rate=0.1)
+db <- dbn.train(db, data= x, n_epocs= 100, n_threads=8)
+
+save.image("~/test_Vowel.RData")
+
+load("~/test_Vowel.RData")
+require(Rdbn)
+
+## Update learning parameters.
+db <- dbn.set_momentum_decay(db, 0.8)
+db <- dbn.set_learning_rate(db, 0.03)
+
+## refine model with new learning parameters.
+db_refine <- dbn.refine(db, data= x, labels= y, n_epocs=100, rate_mult=5, n_threads=1)
+
+val <- dbn.predict(db_refine, data=x, raw_matrix=FALSE)
+mat <- dbn.predict(db_refine, data=x, raw_matrix=TRUE)
+
