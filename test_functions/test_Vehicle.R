96af4b6b08c2a95f1332e745b08b7b169a4110d6
Charles Danko <dankoc@gmail.com>
2013-04-18 21:02:38 -0400

    Minor changes to test fucntions.

--- test_functions/test_Vehicle.R
+++ test_functions/test_Vehicle.R
@@ -10,19 +10,15 @@ for(i in c(1:(NCOL(Vehicle)-1))) {
   x[i,] <- logistic_function(scale(Vehicle[,i])*2) ## 5 is an arbitrarily chosen value... 
 }
 
-trainIndx <- sample(c(1:NCOL(x)), NCOL(x)*0.95, replace=FALSE)
+trainIndx <- sample(c(1:NCOL(x)), NCOL(x)*0.8, replace=FALSE)
 testIndx <- c(1:NCOL(x))[!(c(1:NCOL(x)) %in% trainIndx)]
 
 ## Train a deep belief network.
-db <- dbn(n_layers= 2, layer_sizes= c(18,50), batch_size=100, cd_n=1, momentum_decay= 0.9, learning_rate=0.1, weight_cost= 0.001)
+db <- dbn(n_layers= 3, layer_sizes= c(18,75,50), batch_size=100, cd_n=1, momentum_decay= 0.9, learning_rate=0.1, weight_cost= 0.1)
 db <- dbn.pretrain(db, data= x[,trainIndx], n_epocs= 10, n_threads=8)
 
-## Update learning parameters.
-db <- dbn.set_momentum_decay(db, 0.8)
-db <- dbn.set_learning_rate(db, 0.03)
-
 ## refine model with new learning parameters.
-db_refine <- dbn.refine(db, data= x[,trainIndx], labels= y[trainIndx], n_epocs=100, rate_mult=5, n_threads=1)
+db_refine <- dbn.refine(db, data= x[,trainIndx], labels= y[trainIndx], n_epocs=100, rate_mult=10, n_threads=1)
 pred_dbn <- dbn.predict(db_refine, data=x[,testIndx])
 
 print(paste("% correct (dbn): ", sum(pred_dbn == as.character(y[testIndx]))/NROW(y[testIndx])))
@@ -33,3 +29,4 @@ asvm <- svm(t(x[,trainIndx]), y[trainIndx]) ## SVM does better with RAW data.
 pred_svm <- predict(asvm, t(x[,testIndx]))
 
 print(paste("% correct (svm): ", sum(pred_svm == as.character(y[testIndx]))/NROW(y[testIndx])))
+
