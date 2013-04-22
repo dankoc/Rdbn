843043655385dd52fd03f68229c3387f009dc66f
Charles Danko <dankoc@gmail.com>
2013-04-22 09:16:36 -0400

    test functions.

--- test_functions/test_Vehicle.R
+++ test_functions/test_Vehicle.R
@@ -16,10 +16,9 @@ testIndx <- c(1:NCOL(x))[!(c(1:NCOL(x)) %in% trainIndx)]
 
 ## Train a deep belief network.
 db <- dbn(n_layers= 3, layer_sizes= c(18,75,50), batch_size=100, cd_n=1, momentum_decay= 0.9, learning_rate=0.5, weight_cost= 0.1)
-db <- dbn.pretrain(db, data= x[,trainIndx], n_epocs= 10, n_threads=8)
+db <- dbn.pretrain(db, data= x[,trainIndx], n_epocs= 50, n_threads=8)
 
 ## refine model with new learning parameters.
-set.seed(34)
 
 db_refine <- dbn.refine(db, data= x[,trainIndx], labels= y[trainIndx], n_epocs=100, rate_mult=10, n_threads=8)
 pred_dbn <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
@@ -27,22 +26,22 @@ pred_dbn <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
 print(paste("% correct (dbn): ", sum(pred_dbn == as.character(y[testIndx]))/NROW(y[testIndx])))
 
 ## Compare to SVM
-require(e1071)
-asvm <- svm(t(x[,trainIndx]), y[trainIndx]) ## SVM does better with RAW data.
-pred_svm <- predict(asvm, t(x[,testIndx]))
+# require(e1071)
+# asvm <- svm(t(x[,trainIndx]), y[trainIndx]) ## SVM does better with RAW data.
+# pred_svm <- predict(asvm, t(x[,testIndx]))
 
-print(paste("% correct (svm): ", sum(pred_svm == as.character(y[testIndx]))/NROW(y[testIndx])))
+# print(paste("% correct (svm): ", sum(pred_svm == as.character(y[testIndx]))/NROW(y[testIndx])))
 
 
 
-## BUG ALSO AFFECTS dbn.predict.
- a <- dbn.predict(db_refine, data=x[,testIndx], n_threads=1)
- b <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
- c <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
- d <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
- e <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
+# BUG ALSO AFFECTS dbn.predict.
+ # a <- dbn.predict(db_refine, data=x[,testIndx], n_threads=1)
+ # b <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
+ # c <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
+ # d <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
+ # e <- dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
  
- dbn.predict(db_refine, data=x[,testIndx], n_threads=1) == dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
+ # dbn.predict(db_refine, data=x[,testIndx], n_threads=1) == dbn.predict(db_refine, data=x[,testIndx], n_threads=8)
  
- dbn.predict(db_refine, data=x[,testIndx], n_threads=8, raw_matrix=TRUE)
+ # dbn.predict(db_refine, data=x[,testIndx], n_threads=8, raw_matrix=TRUE)
  
