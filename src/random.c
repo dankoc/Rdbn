e9d7d732d3480cfd8f8519adbf5bd11dfb4a69ad
Charles Danko <dankoc@gmail.com>
2013-07-21 12:45:09 -0400

    Try adding global keyword?!

--- src/random.c
+++ src/random.c
@@ -7,8 +7,8 @@
 
 unsigned long xorshift(void) {
  unsigned long t;
- t=(x^(x>>7)); x=y; y=z; z=w; w=v;
- v=(v^(v<<6))^(t^(t<<13));
+ t=(x_rand^(x_rand>>7)); x_rand=y_rand; y_rand=z_rand; z_rand=w_rand; w_rand=v_rand;
+ v_rand=(v_rand^(v_rand<<6))^(t^(t<<13));
 
- return (y+y+1)*v;
+ return (y_rand+y_rand+1)*v_rand;
 }
--- src/random.h
+++ src/random.h
@@ -2,7 +2,7 @@
 #define rdbn_random_H
 
 /* Fast random number generation... */
-static unsigned long x=123456789,y=362436069,z=521288629,w=88675123,v=886756453;
+static global unsigned long x_rand=123456789,y_rand=362436069,z_rand=521288629,w_rand=88675123,v_rand=886756453;
 unsigned long xorshift(void);
 
 #endif
