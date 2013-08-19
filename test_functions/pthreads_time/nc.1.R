665b96b129cb6b92436e240ce0722140d3ede792
Charles Danko <dankoc@gmail.com>
2013-08-19 08:07:23 -0400

    Updated pthreads.time functions.

--- test_functions/pthreads_time/nc.1.R
+++ test_functions/pthreads_time/nc.1.R
@@ -15,12 +15,12 @@ testIndx <- c(1:NCOL(x))[!(c(1:NCOL(x)) %in% trainIndx)]
 
 ###
 ## Quick & Simple calls to train a classifier using deep belief networks.
-db <- dbn(n_layers= 3, layer_sizes= c(18,75,50), batch_size=100, cd_n=1, momentum_decay= 0.9, learning_rate=0.5, weight_cost= 0.1)
+db <- dbn(n_layers= 3, layer_sizes= c(18,75,150), batch_size=10, cd_n=1, momentum_decay= 0.9, learning_rate=0.1, weight_cost= 0.01)
 db <- dbn.pretrain(db, data= x[,trainIndx], n_epocs= 5, n_threads=1)
 
 ## refine model with new learning parameters.
-db@batch_size <- as.integer(100)
-db_refine <- dbn.refine(db, data= x[,trainIndx], labels= y[trainIndx], n_epocs=100, rate_mult=10, n_threads=1)
+db@batch_size <- as.integer(10)
+db_refine <- dbn.refine(db, data= x[,trainIndx], labels= y[trainIndx], n_epocs=100, rate_mult=5, n_threads=1)
 pred_dbn <- dbn.predict(db_refine, data=x[,testIndx], n_threads=1)
 
 print(paste("% correct (dbn): ", sum(pred_dbn == as.character(y[testIndx]))/NROW(y[testIndx])))
